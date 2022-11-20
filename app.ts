1c7c67453d7c52b2b1841717e505c7a8b7cbb3ce
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-11-20 23:01:55 +0700

    update example

--- app.ts
+++ app.ts
@@ -5,22 +5,46 @@ const input = PromptSync();
 
 /**
  * Example :
- * Print a triangle with amount of rows defined by user
+ * Rock, paper, and scissors game
  */
-let limit;
-while (true) {
-  limit = Number(input("Please enter the number of rows of triangle: "));
-  if (Number.isInteger(limit)) {
-    break;
+const choices = ["rock", "paper", "scissors"];
+
+const computerChoice = () =>
+  choices[Math.floor(Math.random() * choices.length)];
+
+let playerScore = 0;
+let computerScore = 0;
+
+const decideWinner = (player: string, computer: string) =>
+  player === computer
+    ? "tie"
+    : (player === "scissors" && computer === "paper") ||
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock")
+    ? "player"
+    : "computer";
+
+while (playerScore < 3 && computerScore < 3) {
+  const player = input(
+    "What is your pick? rock, paper or scissors: "
+  ).toLowerCase();
+  const computer = computerChoice().toLowerCase();
+
+  if (!choices.includes(player)) {
+    println("Player input must be either rock, paper, or scissors");
+    continue;
   }
-  println("Input must be a number!");
+
+  const winner = decideWinner(player, computer);
+  winner === "player" && playerScore++;
+  winner === "computer" && computerScore++;
+
+  println(
+    `${player} vs ${computer} ${winner === "tie" ? "Tied!" : `${winner} wins!`}`
+  );
+  println(`player score: ${playerScore} computer score: ${computerScore}`);
+  println("------------------------------------");
 }
-for (let i = 1; i <= limit; i++) {
-  for (let j = 1; j <= limit - i; j++) {
-    print(" ");
-  }
-  for (let k = 1; k < i * 2; k++) {
-    print("*");
-  }
-  println();
-}
+
+println(playerScore === 3 ? "Player Wins!" : "Computer wins!");
+println("Game is over");
